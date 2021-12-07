5432e17a705e352cf5412604330013419a0ad4dc
EllipsiaLePoulet <quentin.gavoille@gmail.com>
2021-12-07 17:00:17 +0100

    Waiting for file bottom_left

--- src/client/main.cc
+++ src/client/main.cc
@@ -33,7 +33,15 @@ int main() {
     gf::Window window("Stratego en réseau", windowSize);
     gf::RenderWindow renderer(window);
 
-    gf::Texture texture_land = gf::Texture(gf::Path("./resources/field.png"));
+    gf::Texture land = gf::Texture(gf::Path("./resources/field.png"));
+    gf::Texture land_top_side = gf::Texture(gf::Path("./resources/field_top.png"));
+    gf::Texture land_bot_side = gf::Texture(gf::Path("./resources/field_bottom.png"));   //TODO : Poser la question d'un moyen plus efficace
+    gf::Texture land_right_side = gf::Texture(gf::Path("./resources/field_right.png"));
+    gf::Texture land_left_side = gf::Texture(gf::Path("./resources/field_left.png"));
+    gf::Texture land_top_left_edge = gf::Texture(gf::Path("./resources/field_top_left.png"));
+    gf::Texture land_top_right_edge = gf::Texture(gf::Path("./resources/field_top_right.png"));
+    gf::Texture land_bot_left_edge = gf::Texture(gf::Path("./resources/field_bottom_left.png"));
+    gf::Texture land_bot_right_edge = gf::Texture(gf::Path("./resources/field_bottom_right.png"));
 
     stg::GraphicBoard board;
     for (int i = 0; i < 10; i++) {
@@ -41,7 +49,49 @@ int main() {
         for (int j = 0; j < 10; j++) {
             stg::Tile tile;
             tile.type = stg::Land;
-            tile.sprite.setTexture(texture_land);
+            bool left = false;
+            bool right = false;
+            bool top = false;
+            bool bot = false;
+            if(i==0) {
+                top = true;
+            }
+            if (j==0) {
+                left = true;
+            }
+            if (j==9) {
+                right = true;
+            }
+            if (i==9) {
+                bot = true;
+            }
+            if(left && top) {
+                tile.sprite = gf::Sprite(land_top_left_edge);
+            }
+            else if(left && bot) {
+                tile.sprite = gf::Sprite(land_bot_left_edge);
+            }
+            else if(right && top) {
+                tile.sprite = gf::Sprite(land_top_right_edge);
+            }
+            else if(right && bot) {
+                tile.sprite = gf::Sprite(land_bot_right_edge);
+            }
+            else if(left) {
+                tile.sprite = gf::Sprite(land_left_side);
+            }
+            else if(right) {
+                tile.sprite = gf::Sprite(land_right_side);
+            }
+            else if(top) {
+                tile.sprite = gf::Sprite(land_top_side);
+            }
+            else if(bot) {
+                tile.sprite = gf::Sprite(land_bot_side);
+            }
+            else {
+                tile.sprite = gf::Sprite(land);
+            }
             tile.sprite.setPosition(gf::Vector2i(i * 64,j * 64));
             row.push_back(tile);
         }
