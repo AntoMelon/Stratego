a362ee8f37e4975f1bee255b411f40e26b1c1e8c
AntoMelon <82177158+AntoMelon@users.noreply.github.com>
2022-01-08 13:47:04 +0100

    Moved server board

---
+++ src/server/server_board.h
@@ -0,0 +1,34 @@
+#ifndef STGSERVER_BOARD_H
+#define STGSERVER_BOARD_H
+
+#include "../common/piece.h"
+
+#include <vector>
+
+namespace stg {
+
+    class ServerBoard {
+        public:
+        ServerBoard();
+        bool addPiece(int x, int y, stg::Piece& piece);
+        bool removePiece(int x, int y);
+
+        bool isMoveAllowed(int from_x,int from_y, int to_x, int to_y) const;
+        bool movePiece(int from_x, int from_y, int to_x, int to_y);
+
+
+        stg::Piece getPiece(int x, int y) const;
+        void setPiece(int x, int y, const stg::Piece& piece);
+
+
+        private:
+        std::vector<stg::Piece> board;
+    };
+
+    template<typename Archive>
+    Archive operator|(Archive& ar, stg::ServerBoard& board) {
+        return ar;
+    }
+}
+
+#endif
